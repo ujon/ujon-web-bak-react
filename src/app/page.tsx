1157347ae05632c9980070f4651877f9ea8ef2e9
ujon <ujon94@gmail.com>
2024-10-03 21:13:05 -0700

    fix: typo
    modify xl to lg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     const hero = "Jongho Yoo\nBackend Engineer\nBased in Canada"
     return (
         <main>
-            <section className="flex flex-col justify-center px-8 lg:px-36 xl:hero-h">
+            <section className="flex flex-col justify-center px-8 lg:px-36 lg:hero-h">
                 <p className="text-display-1 whitespace-pre-line">{hero}</p>
                 <hr className="mt-14 mb-12"/>
                 <div className="flex flex-col-reverse justify-between gap-8 lg:flex-row">
@@ -14,7 +14,7 @@ export default function Home() {
                         <ArrowDown className="animate-bounce"/>
                         <p>{t('explore')}</p>
                     </div>
-                    <p className="whitespace-pre-line xl:max-w-96">{t('subtitle')}</p>
+                    <p className="whitespace-pre-line lg:max-w-96">{t('subtitle')}</p>
                 </div>
             </section>
             <section>
