7205d6934ee966f3ff237e40ae8730466a1dc74f
ujon <ujon94@gmail.com>
2024-10-24 15:11:12 -0700

    feat: add maxAge for cookie

--- src/services/locale.ts
+++ src/services/locale.ts
@@ -3,7 +3,7 @@
 import {cookies, headers} from 'next/headers';
 import {defaultLocale, locales, type Locale} from '@/i18n/config';
 
-const COOKIE_NAME: string = 'LOCALE';
+const COOKIE_NAME: string = 'locale';
 
 export async function getUserLocale() {
     const cookieLocale = cookies().get(COOKIE_NAME)?.value;
@@ -14,5 +14,7 @@ export async function getUserLocale() {
 }
 
 export async function setUserLocale(locale: Locale) {
-    cookies().set(COOKIE_NAME, locale);
+    cookies().set(COOKIE_NAME, locale, {
+        maxAge: 60 * 60 * 24 * 365,
+    });
 }
--- src/services/theme.ts
+++ src/services/theme.ts
@@ -3,12 +3,14 @@
 import {cookies} from 'next/headers';
 import {Theme} from "@/common/theme/config";
 
-const COOKIE_NAME: string = 'THEME';
+const COOKIE_NAME: string = 'theme';
 
 export async function getTheme() {
     return cookies().get(COOKIE_NAME)?.value || "system";
 }
 
 export async function setTheme(theme: Theme) {
-    cookies().set(COOKIE_NAME, theme);
+    cookies().set(COOKIE_NAME, theme, {
+        maxAge: 60 * 60 * 24 * 365,
+    });
 }
